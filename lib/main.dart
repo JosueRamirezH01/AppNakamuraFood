0691648dab81c8510102466cc40ed9ae579246a9
josue <josueramirezherrera01@gmail.com>
2024-03-21 16:47:16 -0500

    termine el diseño de login, proceso de la pantalla de productos

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restauflutter/home/home_page.dart';
 import 'package:restauflutter/login/login_page.dart';
 import 'package:restauflutter/home/productos/productos_page.dart';
 
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
       initialRoute: 'home/productos',
       routes: {
         'login': (_) =>  LoginPage(),
-        'home/productos': (_) => ProductosPage()
+        'home/productos': (_) => ProductosPage(),
+        'home':(_)=>HomePage()
       },
     );
   }
