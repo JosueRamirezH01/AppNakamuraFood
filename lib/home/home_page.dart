49a1f4a7274c6f80c85841689965377b46c13d51
christoper__d <91582821+christoper-d@users.noreply.github.com>
2024-03-26 11:34:24 -0500

    Home -> listado de pedidos completado

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -16,45 +16,29 @@ enum Calendar { day, week, month, year }
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   late TabController _tabController;
-  late TabController _tabControllerToSubMenu;
-  late List<bool> _isSelected;
+  int _selectedIndex = 0;
+
   Calendar calendarView = Calendar.week;
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
-    _tabControllerToSubMenu = TabController(length: 3, vsync: this);
-    _isSelected = List.generate(3, (_) => false);
   }
 
   @override
   void dispose() {
     _tabController.dispose();
-    _tabControllerToSubMenu.dispose();
     super.dispose();
   }
 
-  void _updateSelection(int index) {
-    setState(() {
-      for (int buttonIndex = 0;
-          buttonIndex < _isSelected.length;
-          buttonIndex++) {
-        if (buttonIndex == index) {
-          _isSelected[buttonIndex] = true;
-        } else {
-          _isSelected[buttonIndex] = false;
-        }
-      }
-    });
-  }
-
   static final ButtonStyle elevatedButtonStyle = ElevatedButton.styleFrom(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.all(Radius.circular(40)),
-      side: BorderSide(color: Colors.black87, width: 2.0),
-    ),
-    //backgroundColor: Colors.transparent,
-    foregroundColor: Color(0xFF000000),
+    foregroundColor: const Color(0xFF000000),
+    elevation: 5
+  );
+  static final ButtonStyle selectedButtonStyle = ElevatedButton.styleFrom(
+    backgroundColor: const Color(0xFFFF562F), // Cambia el color de fondo cuando está seleccionado
+    foregroundColor: Colors.white, // Cambia el color del texto cuando está seleccionado
+    elevation: 5
   );
 
   @override
@@ -62,77 +46,95 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     return Scaffold(
         appBar: AppBar(
           bottom: PreferredSize(
-            preferredSize: Size.fromHeight(48.0),
+            preferredSize: const Size.fromHeight(48.0),
             child: ButtonBar(
               alignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(
+                ElevatedButton.icon(
                   onPressed: () {
-                    _tabController.animateTo(0);
+                    setState(() {
+                      _selectedIndex = 0;
+                      _tabController.animateTo(0);
+                    });
                   },
-                  child: Text('Listado de pedidos'),
-                  style: elevatedButtonStyle,
+                  icon: Icon(Icons.list_alt_rounded),
+                  label: const Text('Listado de pedidos'),
+                  style: _selectedIndex == 0 ? selectedButtonStyle : elevatedButtonStyle,
                 ),
-                ElevatedButton(
+                ElevatedButton.icon(
                   onPressed: () {
-                    _tabController.animateTo(1);
+                    setState(() {
+                      _selectedIndex = 1;
+                      _tabController.animateTo(1);
+                    });
                   },
-                  child: Text('POS'),
-                  style: elevatedButtonStyle,
+                  icon: Icon(Icons.shopping_cart_outlined),
+                  label: const Text('POS'),
+                  style: _selectedIndex == 1 ? selectedButtonStyle : elevatedButtonStyle,
                 ),
               ],
             ),
           ),
         ),
-        body: ClipRRect(
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30), topRight: Radius.circular(30)),
-            child: Container(
-              color: Color(0xFFD9D9D9), // Fondo de color D9D9D9
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  // Vista para 'Listado de pedidos'
-                  Center(
-                    child: Column(
-                      children: [
-                        Container(
-                          child: subopt(),
-                          margin: EdgeInsets.all(15),
-                        ),
-                        _textFieldSearch(),
-                        //pedidosList()
-                      ],
+        body: Container(
+          margin: EdgeInsets.only(top: 15),
+          child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+              child: Container(
+                color: const Color(0xFFD9D9D9), // Fondo de color D9D9D9
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    // Vista para 'Listado de pedidos'
+                    Center(
+                      child: Column(
+                        children: [
+                          Container(
+                            child: subopt(),
+                            margin: const EdgeInsets.all(15),
+                          ),
+                         Expanded(
+                            child: pedidosList(),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  // Vista para 'POS'
-                  const Center(
-                    child: Column(
-                      children: [
-                        Text('Contenido de POS'),
-                      ],
+                    // Vista para 'POS'
+                    const Center(
+                      child: Column(
+                        children: [
+                          Text('Contenido de POS'),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            )));
+                  ],
+                ),
+              )
+          ),
+        )
+    );
   }
 
   Widget _textFieldSearch() {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 35),
-      child: TextField(
-        decoration: InputDecoration(
-            hintText: 'Buscar',
-            suffixIcon: const Icon(Icons.search, color: Color(0xFF000000)),
-            hintStyle: const TextStyle(fontSize: 15, color: Color(0xFF000000)),
-            enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                borderSide: const BorderSide(color: Colors.grey)),
-            focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                borderSide: const BorderSide(color: Colors.grey)),
-            contentPadding: const EdgeInsets.all(10)),
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        margin: const EdgeInsets.only(top: 20, bottom: 20),
+        child: TextField(
+          decoration: InputDecoration(
+
+              hintText: 'Buscar',
+              suffixIcon: const Icon(Icons.search, color: Color(0xFF000000)),
+              hintStyle: const TextStyle(fontSize: 15, color: Color(0xFF000000)),
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(color: Color(0xFF000000), width: 1.5)),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(color: Color(0xFF000000), width: 1.5)),
+              contentPadding: const EdgeInsets.all(10)),
+        ),
       ),
     );
   }
@@ -141,29 +143,35 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     return SegmentedButton<Calendar>(
       style: SegmentedButton.styleFrom(
         backgroundColor: Colors.grey[200],
-        foregroundColor: Color(0xFF000000),
+        foregroundColor: const Color(0xFF000000),
         selectedForegroundColor: Colors.white,
-        selectedBackgroundColor: Color(0xFFFF562F),
+        selectedBackgroundColor: const Color(0xFFFF562F),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+          side: const BorderSide(color: Color(0xFF000000), width: 2),
+        ),
       ),
 
       segments: const <ButtonSegment<Calendar>>[
         ButtonSegment<Calendar>(
           value: Calendar.day,
           label: SizedBox(
-          width: 53, // Ancho fijo deseado
-          child: Center(child: Text('Local')),
-        ),),
+          width: 53,
+          child: Center(child: Text('Local')
+          ),
+        ),
+        ),
         ButtonSegment<Calendar>(
           value: Calendar.week,
           label: SizedBox(
-            width: 53, // Ancho fijo deseado
+            width: 53,
             child: Center(child: Text('llevar')),
           ),
         ),
         ButtonSegment<Calendar>(
           value: Calendar.month,
           label: SizedBox(
-            width: 53, // Ancho fijo deseado
+            width: 53,
             child: Center(child: Text('Delivery')),
           ),
         ),
@@ -178,16 +186,59 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   }
 
   Widget pedidosList() {
-    return Column(
-      children: [
-        _textFieldSearch(),
-        Expanded(
-          child: Container(
-            child: Text('Aquí empezaremos de cero'),
+    return Container(
+      decoration: const BoxDecoration(
+        color: const Color(0xFF414141),
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: const Color(0xFFD1D1D1),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _textFieldSearch(),
+              const Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Pedido'),
+                    Text('Cliente'),
+                    Text('Estado'),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text('Pedido ${index + 1}'),
+                        subtitle: Text('Cliente ${index + 1}'),
+                        trailing: const Text('Estado'),
+                        onTap: () {
+                          // Lógica cuando se toca un pedido
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
-
 }
