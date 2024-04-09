5852f1a34d477cda1106e9383a428308044240ac
josue <josueramirezherrera01@gmail.com>
2024-04-09 10:42:48 -0500

    page productos

--- lib/home/productos/productos_page.dart
+++ lib/home/productos/productos_page.dart
@@ -20,8 +20,7 @@ class ProductosPage extends StatefulWidget {
 
 class _ProductosPageState extends State<ProductosPage> {
   int estado = 1;
-
- List<Producto>? productosSeleccionados = [];
+  String? nomMesa;
 
   final ProductoController _con = ProductoController();
 
@@ -32,110 +31,111 @@ class _ProductosPageState extends State<ProductosPage> {
     SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
       _con.init(context, refresh);
     });
+
   }
 
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-          length: _con.categorias.length,
-          child: Scaffold(
-          appBar: AppBar(
-            elevation: 3,
-            toolbarHeight: 100,
-            backgroundColor: const Color(0xFF99CFB5),
-            actions: [
-              const SizedBox(width: 5),
-              Expanded(
-                child: Padding(
-                          padding: const EdgeInsets.only(top: 10),
-                          child: ElevatedButton.icon(
-                            style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.deepOrange)),
-                              onPressed: (){
-                              Navigator.pushNamed(context, 'home');
-                              },
-                              icon: const Icon(Icons.arrow_back_ios_outlined, color: Colors.white,),
-                              label: const Text("SALIR", style: TextStyle(fontSize: 18, color: Colors.white),)),
-                        ),
-              ),
-              const SizedBox(width: 50),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: ElevatedButton(
+      length: _con.categorias.length,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 3,
+          toolbarHeight: 100,
+          backgroundColor: const Color(0xFF99CFB5),
+          actions: [
+            const SizedBox(width: 5),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: ElevatedButton.icon(
+                    style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.deepOrange)),
                     onPressed: (){
+                      Navigator.pushNamed(context, 'home');
                     },
-                    child:  Text('${_con.mesa.nombreMesa}', style: TextStyle(fontSize: 18)),),
-                ),
+                    icon: const Icon(Icons.arrow_back_ios_outlined, color: Colors.white,),
+                    label: const Text("SALIR", style: TextStyle(fontSize: 18, color: Colors.white),)),
               ),
-              const SizedBox(width: 5)
-            ],
-            bottom: TabBar(
-              isScrollable: true,
-              tabs: List<Widget>.generate(_con.categorias.length, (index) {
-                return Tab(
-                  child: Text(_con.categorias[index].nombre ?? ''),
-                );
-              }),
-              onTap: (index) async {
-                List<Producto> productosCategoria = await _con.getProductosPorCategoria(_con.categorias[index].id);
-                setState(() {
-                  _con.productos = productosCategoria;
-                });
-              },
             ),
-          ),
-          body:  Column(
-            children: [
-              const SizedBox(height: 10),
-              _textFieldSearch(),
-              const SizedBox(height: 10),
-              Expanded(
-                child: TabBarView(
-                  children: _con.categorias.map((categoria)  {
-                    return  GridView.builder(
-                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            childAspectRatio: 0.7
-                        ),
-                        itemCount: _con.productos.length,
-                        itemBuilder: (_, index) {
-                          if (index < _con.productos.length) {
-                            Producto producto = _con.productos[index];
-                            return _cardProduct(producto);
-                          } else {
-                            return const SizedBox(); // Otra opción es devolver un widget vacío si el índice está fuera de rango
-                          }
-                        }
-                    );
-                  }).toList(),
-                ),
-              ),
-            ],
-          ),
-            floatingActionButton: Container(
-              width: MediaQuery.of(context).size.width,
-              alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                width: 250,
-                height: 50,
+            const SizedBox(width: 50),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10),
                 child: ElevatedButton(
-                  onPressed: () {
-                    pedido();
+                  onPressed: (){
                   },
-                  child:  Row(
-                    children: [
-                      Expanded(child: cantidad()),
-                      const SizedBox(width: 10),
-                      Expanded(child: precio()),
-                    ],
-                  ),
-                ),
+                  child:  Text('${nomMesa ?? _con.mesa.nombreMesa}', style: TextStyle(fontSize: 18)),),
               ),
             ),
-            floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterFloat,
+            const SizedBox(width: 5)
+          ],
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: List<Widget>.generate(_con.categorias.length, (index) {
+              return Tab(
+                child: Text(_con.categorias[index].nombre ?? ''),
+              );
+            }),
+            onTap: (index) async {
+              List<Producto> productosCategoria = await _con.getProductosPorCategoria(_con.categorias[index].id);
+              setState(() {
+                _con.productos = productosCategoria;
+              });
+            },
           ),
+        ),
+        body:  Column(
+          children: [
+            const SizedBox(height: 10),
+            _textFieldSearch(),
+            const SizedBox(height: 10),
+            Expanded(
+              child: TabBarView(
+                children: _con.categorias.map((categoria)  {
+                  return  GridView.builder(
+                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 0.7
+                      ),
+                      itemCount: _con.productos.length,
+                      itemBuilder: (_, index) {
+                        if (index < _con.productos.length) {
+                          Producto producto = _con.productos[index];
+                          return _cardProduct(producto);
+                        } else {
+                          return const SizedBox(); // Otra opción es devolver un widget vacío si el índice está fuera de rango
+                        }
+                      }
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: Container(
+          width: MediaQuery.of(context).size.width,
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            width: 250,
+            height: 50,
+            child: ElevatedButton(
+              onPressed: () {
+                pedido();
+              },
+              child:  Row(
+                children: [
+                  Expanded(child: cantidad()),
+                  const SizedBox(width: 10),
+                  Expanded(child: precio()),
+                ],
+              ),
+            ),
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterFloat,
+      ),
 
     );
   }
@@ -151,8 +151,8 @@ class _ProductosPageState extends State<ProductosPage> {
 
   double calcularTotal() {
     double total = 0;
-    if (productosSeleccionados != null) {
-      for (Producto producto in productosSeleccionados!) {
+    if (_con.productosSeleccionados != null) {
+      for (Producto producto in _con.productosSeleccionados!) {
         total += (producto.precioproducto ?? 0) * (producto.stock ?? 0);
       }
     }
@@ -160,8 +160,8 @@ class _ProductosPageState extends State<ProductosPage> {
   }
   int calcularCantidadProductosSeleccionados() {
     int cantidad = 0;
-    if (productosSeleccionados != null) {
-      for (Producto producto in productosSeleccionados!) {
+    if (_con.productosSeleccionados != null) {
+      for (Producto producto in _con.productosSeleccionados!) {
         cantidad += producto.stock ?? 0;
       }
     }
@@ -170,120 +170,129 @@ class _ProductosPageState extends State<ProductosPage> {
 
   void _actualizarProductosSeleccionados(List<Producto>? nuevosProductosSeleccionados) {
     setState(() {
-      productosSeleccionados = nuevosProductosSeleccionados;
+      _con.productosSeleccionados = nuevosProductosSeleccionados;
     });
   }
 
 
 
-  Future pedido(){
-    return showCupertinoModalBottomSheet(
+  Future pedido() async {
+    List<Producto>? productosSeleccionadosCopy = List.from(_con.productosSeleccionados ?? []);
+    nomMesa = await showCupertinoModalBottomSheet<String?>(
       barrierColor: Colors.transparent,
       context: context,
       builder: (BuildContext context) {
         return SingleChildScrollView(
           child: SizedBox(
             height: MediaQuery.of(context).size.height * 0.87,
-            child:  DetailsPage(
-                productosSeleccionados: productosSeleccionados,
-                estado: estado,
-                onProductosActualizados: _actualizarProductosSeleccionados,
+            child: DetailsPage(
+              productosSeleccionados: productosSeleccionadosCopy,
+              productosSeleccionadosOtenidos: _con.productosSeleccionadosOtenidos,
+              mesa: _con.mesa,
+              onProductosActualizados: _actualizarProductosSeleccionados,
             ),
           ),
         );
       },
     );
+
+    // Haz lo que necesites con el valor de nomMesa, por ejemplo, imprimirlo
+    print('Valor de nomMesa: $nomMesa');
   }
 
 
   Widget _cardProduct(Producto producto) {
-   return GestureDetector(
-     onTap: () {
-       setState(() {
-         if (productosSeleccionados?.contains(producto) ?? false) {
-           // El producto ya está seleccionado, muestra un mensaje de confirmación
-           _con.mostrarMensaje('El producto ya ha sido seleccionado.');
-         } else {
-           // El producto no está seleccionado, agrégalo a la lista de productos seleccionados
-           setState(() {
-             productosSeleccionados?.add(producto);
-           });
-           // Muestra un mensaje de confirmación
-          _con.agregarMsj('El producto se ha añadido a la lista.');
-         }
-       });
-     },
-     child: SizedBox(
-       child: Card(
-         elevation: 3.0,
-         shape: RoundedRectangleBorder(
-             borderRadius: BorderRadius.circular(15)
-         ),
-         child: Stack(
-           children: [
-             Positioned(
-                 bottom: double.minPositive,
-                 right: -1.0,
-                 child: Container(
-                   width: 40,
-                   height: 40,
-                   decoration: const BoxDecoration(
-                       color: Colors.green,
-                       borderRadius: BorderRadius.only(
-                         bottomLeft: Radius.circular(10),
-                         topLeft: Radius.circular(15),
-                       )
-                   ),
-                   child: const Icon(Icons.add, color: Colors.white,),
-                 )
-             ),
-             Column(
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
-                 Container(
-                   height: MediaQuery.of(context).size.height * 0.2,
-                   width: MediaQuery.of(context).size.width,
-                   padding: const EdgeInsets.only(left: 10, right: 10),
-                   child: const FadeInImage(
-                     image: AssetImage('assets/img/arrozconpollo.jpeg'),
-                     fit: BoxFit.contain,
-                     fadeInDuration: Duration(milliseconds: 50),
-                     placeholder: AssetImage('assets/img/no-image.png'),
-                   ),
-                 ),
-                 Container(
-                   margin: const EdgeInsets.symmetric(horizontal: 20),
-                   height: 40,
-                   child:  Text(
-                     producto.nombreproducto ?? 'Nombre no disponible',
-                     maxLines: 2,
-                     overflow: TextOverflow.ellipsis,
-                     style: const TextStyle(
-                         fontSize: 15,
-                         fontFamily: 'NimbusSans'
-                     ),
-                   ),
-                 ),
-                 const SizedBox(height: 10),
-                 Container(
-                   margin: const EdgeInsets.symmetric(horizontal: 20),
-                   child: Text(
-                     'S/ ${producto.precioproducto ?? 'Precio no disponible'} ' ,
-                     style: const TextStyle(
-                         fontSize: 15,
-                         fontWeight: FontWeight.bold,
-                         fontFamily: 'NimbusSans'
-                     ),
-                   ),
-                 )
-               ],
-             )
-           ],
-         ),
-       ),
-     ),
-   );
- }
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          if (_con.productosSeleccionados?.any((p) => p.nombreproducto == producto.nombreproducto) ?? false) {
+            // El producto ya está seleccionado, muestra un mensaje de confirmación
+            _con.mostrarMensaje('El producto ya ha sido seleccionado.');
+          } else {
+            // El producto no está seleccionado, agrégalo a la lista de productos seleccionados
+            setState(() {
+              _con.productosSeleccionados?.add(producto);
+            });
+            // Muestra un mensaje de confirmación
+            _con.agregarMsj('El producto se ha añadido a la lista.');
+            print('Productos seleccionados:');
+            _con.productosSeleccionados?.forEach((prod) {
+              print(prod.nombreproducto);
+            });
+          }
+        });
+      },
+      child: SizedBox(
+        child: Card(
+          elevation: 3.0,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15)
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                  bottom: double.minPositive,
+                  right: -1.0,
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: const BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          topLeft: Radius.circular(15),
+                        )
+                    ),
+                    child: const Icon(Icons.add, color: Colors.white,),
+                  )
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                    width: MediaQuery.of(context).size.width,
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: const FadeInImage(
+                      image: AssetImage('assets/img/arrozconpollo.jpeg'),
+                      fit: BoxFit.contain,
+                      fadeInDuration: Duration(milliseconds: 50),
+                      placeholder: AssetImage('assets/img/no-image.png'),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    height: 40,
+                    child:  Text(
+                      producto.nombreproducto ?? 'Nombre no disponible',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                          fontSize: 15,
+                          fontFamily: 'NimbusSans'
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      'S/ ${producto.precioproducto ?? 'Precio no disponible'} ' ,
+                      style: const TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'NimbusSans'
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
 
   Widget _textFieldSearch() {
