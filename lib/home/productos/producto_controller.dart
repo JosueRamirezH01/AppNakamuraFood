7cfe08c81bceb7132185d5a01e0a7ceded742de1
josue <josueramirezherrera01@gmail.com>
2024-04-09 10:15:26 -0500

    controller productos

--- lib/home/productos/producto_controller.dart
+++ lib/home/productos/producto_controller.dart
@@ -7,6 +7,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:restauflutter/model/categoria.dart';
+import 'package:restauflutter/model/detalle_pedido.dart';
+import 'package:restauflutter/model/mesaDetallePedido.dart';
 import 'package:restauflutter/utils/shared_pref.dart';
 
 import '../../model/mesa.dart';
@@ -20,15 +22,92 @@ class ProductoController {
   late Timer searchOnStoppedTyping = Timer(Duration.zero, () {});
   String productName = '';
   late  Mesa mesa =  Mesa();
+  late List<Detalle_Pedido> detalle_pedido = [];
+
+  List<Producto>? productosSeleccionadosOtenidos = [];
+  List<Producto>? productosSeleccionados = [];
   Future init(BuildContext context, Function refresh) async {
     this.context = context;
     this.refresh = refresh;
     _getProductos();
     _getCategorias();
-     mesa = ModalRoute.of(context)?.settings.arguments as Mesa;
-     print('ESTADO DE MESA ${mesa.estadoMesa}');
+    final args = ModalRoute.of(context)?.settings.arguments;
+    if (args is Mesa) {
+      mesa = args;
+      print('ESTADO DE MESA ${mesa.estadoMesa}');
+    }
+    // Verifica si los argumentos son de tipo List<Detalle_Pedido>
+    else if (args is MesaDetallePedido) {
+      mesa = args.mesa;
+      detalle_pedido = args.detallePedido;
+      // Itera sobre los detalles del pedido, si es necesario
+
+      detalle_pedido.forEach((detalle) async {
+        // Busca el producto correspondiente al detalle
+        Producto? producto = await _getProductoPorId(detalle.id_producto);
+        Producto? setproducto = Producto(
+          nombreproducto: producto?.nombreproducto,
+          precioproducto: producto?.precioproducto,
+          stock: detalle.cantidad_producto,
+          comentario: detalle.comentario,
+          idPedido: detalle.id_pedido
+        );
+        print('NOMBRE PRODUCTO ${producto?.nombreproducto}');
+        print('CANTIDAD PRODUCTO ${detalle.cantidad_producto}');
+        print('PRECIO PRODUCTO ${detalle.precio_producto}');
+
+        if (producto != null) {
+          // Agrega el producto a la lista de productos encontrados
+          productosSeleccionados?.add(setproducto);
+        }
+      });
+    }
+    obtenerLista();
     refresh();
+
   }
+
+  void obtenerLista(){
+    detalle_pedido.forEach((detalle) async {
+      // Busca el producto correspondiente al detalle
+      Producto? producto = await _getProductoPorId(detalle.id_producto);
+      Producto? setproducto = Producto(
+          nombreproducto: producto?.nombreproducto,
+          precioproducto: producto?.precioproducto,
+          stock: detalle.cantidad_producto,
+          comentario: detalle.comentario,
+          idPedido: detalle.id_pedido
+      );
+      print('NOMBRE PRODUCTO ${producto?.nombreproducto}');
+      print('CANTIDAD PRODUCTO ${detalle.cantidad_producto}');
+      print('PRECIO PRODUCTO ${detalle.precio_producto}');
+
+      if (producto != null) {
+        productosSeleccionadosOtenidos?.add(setproducto);
+        print('INIT ${productosSeleccionadosOtenidos![0].stock}');
+      }
+    });
+  }
+
+  Future<Producto?> _getProductoPorId(int? idProducto) async {
+    try {
+      String productosJson = await _sharedPref.read('productos');
+      if (productosJson.isNotEmpty) {
+        List<dynamic> productosData = json.decode(productosJson);
+        // Busca el producto con el ID dado
+        for (var productoJson in productosData) {
+          Producto producto = Producto.fromJson(productoJson);
+          if (producto.id == idProducto) {
+            return producto;
+          }
+        }
+      }
+    } catch (e) {
+      print('Error al obtener el producto por ID: $e');
+    }
+    return null; // Devuelve null si no se encuentra el producto
+  }
+
 
 
   void onChangeText(String text) {
@@ -135,5 +214,4 @@ class ProductoController {
     );
   }
 }
-
 
