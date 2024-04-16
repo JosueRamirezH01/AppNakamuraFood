eafef5996621a58dabb66d1b8cc5ddc9529c122c
JosueRamirezH01 <52433389+JosueRamirezH01@users.noreply.github.com>
2024-04-16 16:21:33 -0500

    Update detalle_pedido_service.dart

--- lib/services/detalle_pedido_service.dart
+++ lib/services/detalle_pedido_service.dart
@@ -79,130 +79,7 @@ class DetallePedidoServicio {
     }
   }
 
-  Future<List<Detalle_Pedido>> actualizarCantidadProductoDetallePedido(int? pedidoid,List<Detalle_Pedido> detalle_pedidos, List<Producto> productos, double pedidoTotal, BuildContext context) async {
-    MySqlConnection? conn;
-    try {
-      int? idpedido = 0;
 
-      conn = await _connectionSQL.getConnection();
-
-      if(pedidoid == 0){
-        idpedido = detalle_pedidos[0].id_pedido;
-        print('PEDIDO REFERSH$idpedido');
-      }else{
-        idpedido = pedidoid;
-      }
-
-      print('PEDIDOID $idpedido');
-
-      Set<int?> idsProductosEnDetalle = detalle_pedidos.map((detalle) => detalle.id_pedido_detalle).toSet();
-      final existingDetailCombined = [];
-      for (Producto producto in productos) {
-        final existingDetail = await conn.query('''
-        SELECT id_pedido_detalle
-        FROM pedido_detalles
-        WHERE id_pedido = ?
-        AND id_producto = ?
-      ''', [idpedido, producto.id]);
-        existingDetailCombined.addAll(existingDetail);
-
-        if (existingDetail.isEmpty) {
-          final results = await conn.query('''
-          INSERT INTO pedido_detalles (
-              id_pedido,
-              id_producto,
-              cantidad_producto,
-              cantidad_real,
-              precio_producto,
-              comentario,
-              estado_detalle,
-              created_at,
-              updated_at
-          ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''', [
-            idpedido, // id pedido
-            producto.id, // id producto
-            producto.stock, // cantidad producto
-            producto.stock, // cantidad real
-            producto.precioproducto, // precio producto
-            producto.comentario, // comentario
-            1, // estado detalle
-            DateTime.now().toUtc(),
-            DateTime.now().toUtc()
-          ]);
-          idsProductosEnDetalle.add(producto.id);
-        } else {
-          double precio = (producto.precioproducto! * producto.stock!);
-          print('STOCK ${producto.stock}');
-          await conn.query('''
-          UPDATE pedido_detalles 
-          SET cantidad_producto = ?,
-              cantidad_real = ?,
-              precio_producto = ?,
-              comentario = ?,
-              updated_at = ?
-          WHERE id_pedido = ? AND id_producto = ?
-        ''', [
-            producto.stock,
-            producto.stock,
-            precio,
-            producto.comentario,
-            DateTime.now().toUtc(),
-            idpedido,
-            producto.id
-          ]);
-        }
-
-        final existingDetailDespues = await conn.query('''
-        SELECT id_pedido_detalle
-        FROM pedido_detalles
-        WHERE id_pedido = ?
-        AND id_producto = ?
-      ''', [idpedido, producto.id]);
-        existingDetailCombined.addAll(existingDetailDespues);
-        print('IDE existingDetailDespues $existingDetailDespues');
-        List<int?> existingIds = existingDetailCombined.map((
-            row) => row['id_pedido_detalle'] as int?).toList();
-        print('LISTA DE ID COMBINADA $existingIds');
-        int? missingId;
-        for (Detalle_Pedido detalle in detalle_pedidos) {
-          print('ID DETALLE DE PEDIDO ${detalle.id_pedido_detalle}');
-          if (!existingIds.contains(detalle.id_pedido_detalle)) {
-            missingId = detalle.id_pedido_detalle;
-            print('IDE QUE FALTA $missingId');
-            break;
-          }
-        }
-
-        if (missingId != null) {
-          await conn.query('''
-    DELETE FROM pedido_detalles WHERE id_pedido_detalle = ?
-  ''', [missingId]);
-          print('Se eliminó el registro con ID: $missingId');
-        } else {
-          print('No hay registros para eliminar');
-        }
-      }
-
-      await conn.query('''
-      UPDATE pedidos SET Monto_total=?  WHERE id_pedido = ?
-    ''', [pedidoTotal, idpedido]);
-
-      final results = await conn.query('''
-      SELECT * FROM pedido_detalles WHERE id_pedido = ?
-      ''', [idpedido]);
-
-      List<Detalle_Pedido> detalle_pedido_actualizado = results.map((row) => Detalle_Pedido.fromJson(row.fields)).toList();
-
-      print('Cantidad actualizada correctamente en los detalles de pedido');
-      return detalle_pedido_actualizado;
-    } catch (e) {
-      print('Error al actualizar la cantidad del producto en los detalles de pedido: $e');
-      return [];
-    } finally {
-      await conn?.close();
-    }
-  }
 
   Future<List<Detalle_Pedido>> actualizarCantidadProductoDetallePedidoPrueba(
       int? pedidoid,
@@ -248,9 +125,9 @@ class DetallePedidoServicio {
 
       for (final producto in productos) {
         var existingDetail = await conn.query(
-            'SELECT id_pedido_detalle FROM pedido_detalles WHERE id_pedido = ? AND id_producto = ?',
+            'SELECT id_pedido_detalle, cantidad_producto FROM pedido_detalles WHERE id_pedido = ? AND id_producto = ?',
             [pedidoid, producto.id]);
-
+        print('LISTA $existingDetail');
         if (existingDetail.isEmpty) {
 
           Detalle_Pedido nuevoDetalle = Detalle_Pedido(
@@ -278,9 +155,27 @@ class DetallePedidoServicio {
               ]);
           detallesPedido.add(nuevoDetalle);
         } else {
+
+          var detailRow = existingDetail.first;
+          int cantidadProductoExistente = detailRow['cantidad_producto'];
+          int productoRestado = producto.stock! - cantidadProductoExistente;
+          print('Cantidad Restado $productoRestado');
+          if(productoRestado > 0){
+            Detalle_Pedido nuevoDetalle2 = Detalle_Pedido(
+              id_pedido: pedidoid,
+              id_producto: producto.id,
+              cantidad_producto: productoRestado,
+              cantidad_real: producto.stock,
+              precio_producto: producto.precioproducto,
+              comentario: producto.comentario,
+              estado_detalle: 1,
+            );
+            detallesPedido.add(nuevoDetalle2);
+          }
+
+
+
           double precio = producto.precioproducto! * producto.stock!;
-
-
           await conn.query(
               'UPDATE pedido_detalles SET cantidad_producto = ?, cantidad_real = ?, precio_producto = ?, comentario = ?, updated_at = ? WHERE id_pedido = ? AND id_producto = ?',
               [
@@ -292,8 +187,6 @@ class DetallePedidoServicio {
                 pedidoid,
                 producto.id
               ]);
-
-
         }
 
       }
